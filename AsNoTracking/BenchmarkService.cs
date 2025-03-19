87f55d6be9d668f5110273fabb13bf3cfa075ccb
taberkkaya <taberkkaya@gmail.com>
2025-03-19 09:06:29 +0300

    as no tracking

---
+++ AsNoTracking/BenchmarkService.cs
@@ -0,0 +1,47 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsNoTracking;
+
+public class BenchmarkService
+{
+    [ShortRunJob, Config(typeof(Config))]
+    private class Config : ManualConfig
+    {
+        public Config()
+        {
+            SummaryStyle = BenchmarkDotNet.Reports.SummaryStyle.Default.WithRatioStyle(
+                BenchmarkDotNet.Columns.RatioStyle.Trend);
+        }
+    }
+
+    //[Benchmark(Baseline = true)]
+    //public void GetAllWithTracking()
+    //{
+    //    AppDbContext context = new();
+    //    context.Products.ToList();
+    //}
+
+    //[Benchmark]
+    //public void GetAllWithAsNoTracking()
+    //{
+    //    AppDbContext context = new();
+    //    context.Products.AsNoTracking().ToList();
+    //}
+
+    [Benchmark(Baseline = true)]
+    public void GetFirstWithTracking()
+    {
+        AppDbContext context = new();
+        context.Products.First();
+    }
+
+    [Benchmark]
+    public void GetFirstWithAsNoTracking()
+    {
+        AppDbContext context = new();
+        context.Products.AsNoTracking().First();
+    }
+}
