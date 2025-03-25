0f4d7f7a12e1c5124d2357cbe151bb503f936ffd
taberkkaya <taberkkaya@gmail.com>
2025-03-25 21:49:52 +0300

    Record

---
+++ Record/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Record.Dtos;
+
+namespace Record.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+    [HttpGet("[action]")]
+    public IActionResult Login(LoginDto loginDto)
+    {
+        LoginDto loginDto2 = new()
+        {
+            Email = "taberkkaya@gmail.com",
+            Password = "1",
+            RememberMe = true
+        };
+
+        loginDto.Email = string.Empty;
+        return Ok("Giriş başarılı.");
+    }    
+    
+    [HttpGet("[action]")]
+    public IActionResult LoginWithRecord(Login login)
+    {
+        Login login2 = new("taberkkaya@gmail.com", "1" , true);
+        loginDto.Email = string.Empty;
+        loginDto2.Email = string.Empty;
+        return Ok("Giriş başarılı.");
+    }
+}
---
+++ Record/Dtos/LoginDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Record.Dtos;
+
+public class LoginDto
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public bool RememberMe { get; set; }
+}
+
+public record Login(
+    string Email,
+    string Password,
+    bool RememberMe = true);
