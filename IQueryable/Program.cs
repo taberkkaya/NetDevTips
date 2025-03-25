01850bed08d962a215a18f0981ef57405597f428
taberkkaya <taberkkaya@gmail.com>
2025-03-25 21:43:24 +0300

    IQueryable

---
+++ IQueryable/Program.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Context;
+using DataAccess.Models;
+
+namespace IQueryable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDbContext context = new();
+            IQueryable<Product> products = context.Products.AsQueryable();
+
+            IList<Product> productList = products.Where(p => p.Id > 50000 && p.Id < 60000).ToList();
+
+            
+        }
+    }
+}
