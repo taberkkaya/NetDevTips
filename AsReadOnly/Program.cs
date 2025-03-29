ab7899a5aff02dbae1586192c58d38daf61b054f
taberkkaya <taberkkaya@gmail.com>
2025-03-29 03:16:57 +0300

    AsReadOnly

---
+++ AsReadOnly/Program.cs
@@ -0,0 +1,27 @@
+﻿using DataAccess.Context;
+using DataAccess.Models;
+
+namespace AsReadOnly;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        AppDbContext context = new();
+        //IList<Product> products = context.Products.ToList();
+        IReadOnlyList<Product> products = context.Products.ToList().AsReadOnly();
+
+        products[0].Name = "Test";
+
+        Console.WriteLine(products[0].Name);
+
+        //Product product = new()
+        //{
+        //    Name = "Test2"
+        //};
+
+        //products.Add(product);
+
+
+    }
+}
