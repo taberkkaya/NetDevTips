4069b0699882d9f7e4b958fcff4392dd5ebbe0b1
taberkkaya <taberkkaya@gmail.com>
2025-03-29 03:44:39 +0300

    Required Member

---
+++ RequiredMember/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace RequiredMember;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Product product = new();
+        //Product product = new("Test");
+        Product product = new() { Name = "Test" };
+    }
+
+    public class Product
+    {
+        //public Product(string Name)
+        //{
+        //    Name = Name;
+        //}
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
