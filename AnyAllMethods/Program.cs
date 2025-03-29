93a4d033f5e59cc6112e3e829c49f23e187bc5fd
taberkkaya <taberkkaya@gmail.com>
2025-03-29 03:39:18 +0300

    Any-All

---
+++ AnyAllMethods/Program.cs
@@ -0,0 +1,20 @@
+﻿using DataAccess.Context;
+
+namespace AnyAllMethods;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        AppDbContext context = new();
+        
+        bool result = context.Products.Any();
+
+        bool result2 = context.Products.Any(x => x.Name == "Product 1");
+
+        bool result3 = context.Products.All(x => x.Name.Contains("P")); //true
+        bool result4 = context.Products.All(x => x.Name.Contains("Product 1")); //false
+
+        Console.WriteLine("Hello, World!");
+    }
+}
